655822690e497ccc04a185e8de5c8f957935bfff
chen-qingyu <chen_qingyu@qq.com>
2026-05-21 10:51:16 +0800

    chore: 更新 .gitignore 和构建配置

--- xmake.lua
+++ xmake.lua
@@ -7,7 +7,25 @@ set_encodings("utf-8")
 add_rules("mode.debug", "mode.release")
 add_requires("nlohmann_json 3.12", "spdlog 1.17", "catch2 3.14", "argparse 3.2")
 
+target("hypercube-lib")
+    set_kind("static")
+    add_packages("nlohmann_json", "spdlog", {public = true})
+    add_files("src/core/*.cpp")
+    add_includedirs("src", {public = true})
+    if is_plat("linux") then
+        add_cxflags("-fPIC")
+    end
+
 target("hypercube")
     set_kind("binary")
     set_rundir(".")
+    add_packages("argparse")
+    add_deps("hypercube-lib")
     add_files("src/main.cpp")
+
+target("test")
+    set_kind("binary")
+    set_rundir(".")
+    add_deps("hypercube-lib")
+    add_packages("catch2")
+    add_files("tests/*.cpp")
