94bc4595d5857b0aa33e105f926d8c36ea6f8d70
chen-qingyu <chen_qingyu@qq.com>
2026-06-18 06:18:36 +0800

    refac: refine xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -10,8 +10,7 @@ add_requires("spdlog 1.17", "nlohmann_json 3.12", "json-schema-validator 2.4", "
 target("core")
     set_kind("static")
     add_packages("spdlog", "nlohmann_json", "json-schema-validator", {public = true})
-    add_files("src/core/*.cpp", "src/core/algorithm/sgep/*.cpp", "src/core/algorithm/mlhs/*.cpp")
-    add_includedirs("src", {public = true})
+    add_files("src/core/**.cpp")
     if is_plat("linux") then
         add_cxflags("-fPIC")
     end
@@ -29,9 +28,11 @@ target("test")
     add_deps("core")
     add_packages("catch2")
     add_files("tests/*.cpp")
+    add_includedirs("src")
 
 target("report")
     set_kind("binary")
     set_rundir(".")
     add_deps("core")
     add_files("report/*.cpp")
+    add_includedirs("src")
